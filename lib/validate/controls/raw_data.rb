12d8859daf54d32f964b46056f45937d5b9d83f5
Scott Bellware <sbellware@gmail.com>
2016-04-28 18:18:51 -0500

    Skeleton of project

--- lib/validate/controls/raw_data.rb
+++
@@ -1,9 +0,0 @@
-module Serialize
-  module Controls
-    module RawData
-      def self.example
-        'some raw data'
-      end
-    end
-  end
-end
--- lib/validate/controls/text.rb
+++
@@ -1,9 +0,0 @@
-module Serialize
-  module Controls
-    module Text
-      def self.example
-        'some text'
-      end
-    end
-  end
-end
