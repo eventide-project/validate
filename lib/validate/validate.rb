d2a16d8e40d6277ac1f7a854cce1336bfd06e5c7
Scott Bellware <sbellware@gmail.com>
2016-04-29 17:42:49 -0500

    Specialized validator retrieval

--- lib/validate/validate.rb
+++ lib/validate/validate.rb
@@ -4,22 +4,27 @@ module Validate
   class Error < RuntimeError; end
 
   def call(subject, specialization=nil)
-    validator = validator(subject, specialization)
+    validator = validator(subject)
+
+    if specialization
+      validator = specialization(validator, specialization)
+    end
+
     validator.(subject)
   end
 
-  def validator(subject, specialization)
+  def validator(subject)
     subject_const = subject_const(subject)
 
-    assure_validator(subject_const, specialization)
-    get_validator(subject_const, specialization)
+    assure_validator(subject_const)
+    get_validator(subject_const)
   end
 
   def subject_const(subject)
     [Module, Class].include?(subject.class) ? subject : subject.class
   end
 
-  def assure_validator(subject_const, specialization)
+  def assure_validator(subject_const)
     unless validator_const?(subject_const)
       raise Error, "#{subject_const.name} doesn't have a `Validator' namespace"
     end
@@ -29,7 +34,26 @@ module Validate
     subject_const.const_defined?(:Validator)
   end
 
-  def get_validator(subject_const, specialization)
+  def get_validator(subject_const)
     subject_const.const_get(:Validator)
+  end
+
+  def specialization(validator, specialization)
+    assure_specialization(validator, specialization)
+    get_specialization(validator, specialization)
+  end
+
+  def assure_specialization(validator, specialization)
+    unless specialization_method?(validator, specialization)
+      raise Error, "#{validator.name} doesn't have a `#{specialization}' specialization method"
+    end
+  end
+
+  def specialization_method?(validator, specialization)
+    validator.respond_to?(specialization)
+  end
+
+  def get_specialization(validator, specialization)
+    validator.public_send(specialization)
   end
 end
