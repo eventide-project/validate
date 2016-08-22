3ea9047bc8122b478a89ee496b340e4ca4ab8b62
Scott Bellware <sbellware@gmail.com>
2016-08-22 12:57:58 -0700

    Inline format appears clearer

--- test/bench/state_object/collect_state.rb
+++ test/bench/state_object/collect_state.rb
@@ -5,37 +5,10 @@ context "Collect State in Block Argument" do
 
   state = OpenStruct.new
 
-  result = Validate.(example) do |state|
+  s = nil
+  result = Validate.(example) { |state| s = state }
 
-    test "State is collected by state object" do
-      assert(state.entries.first == :some_entry)
-    end
-
+  test "State is collected by state object" do
+    assert(s.entries.first == :some_entry)
   end
 end
-
-__END__
-
-
-s = nil
-result = Validate.(example) { |state| s = state }
-
-- - -
-
-s = State.new
-Validate.(example, s)
-
-- - -
-
-invoke call
-get state
-pass it to Block
-return boolean
-
-
-class actuator takes block
-- constructs state object
-- passess it to block
-
-instance actuator takes state arg
-- invokes it
